618ccbeb00aa22320ee302b165f5a917da4daabd
Kikyung Kim <overthezenith@gmail.com>
2020-09-24 07:20:33 +0900

    refactoring inner structure
    - gathered UI handling functions into single structure.

--- pinlight.js
+++ pinlight.js
@@ -80,118 +80,119 @@ gStatus = {
     }
 };
 
-function onClickEdit() {
-    gStatus.pauseTimer();
-
-    document.getElementById("edit-subject").value = gConfig.subject;
-    document.getElementById("edit-cooldown").value = gConfig.cooldownSpanMin;
-    document.getElementById("edit-sprint").value = gConfig.sprintSpanMin;
-
-    document.getElementById("watch").style.display = "none";
-    document.getElementById("edit").style.display = "block";
-}
-
-function onClickEditDone() {
-    let subject = document.getElementById("edit-subject").value;
-    let cooldownSpanMin = parseInt(document.getElementById("edit-cooldown").value);
-    let sprintSpanMin = parseInt(document.getElementById("edit-sprint").value);
-
-    if (subject != gConfig.subject || cooldownSpanMin != gConfig.cooldownSpanMin || sprintSpanMin != gConfig.sprintSpanMin) {
-        gConfig.subject = subject;
-        gConfig.cooldownSpanMin = cooldownSpanMin;
-        gConfig.sprintSpanMin = sprintSpanMin;
-
-        updateSubjectElem();
-        gStatus.prepareSprint();
-    }
-
-    document.getElementById("edit").style.display = "none";
-    document.getElementById("watch").style.display = "block";
-
-    gStatus.startTimer();
-}
-
-function onClickPause() {
-    if (gStatus.ticker == null) {
-        gStatus.startTimer();
-        document.getElementById("button-pause").innerHTML = "=";
-    } else {
+// frontend stuff.
+gFrontend = {
+    onClickEdit: function() {
         gStatus.pauseTimer();
-        document.getElementById("button-pause").innerHTML = ">";
+
+        document.getElementById("edit-subject").value = gConfig.subject;
+        document.getElementById("edit-cooldown").value = gConfig.cooldownSpanMin;
+        document.getElementById("edit-sprint").value = gConfig.sprintSpanMin;
+
+        document.getElementById("watch").style.display = "none";
+        document.getElementById("edit").style.display = "block";
+    },
+
+    onClickEditDone: function() {
+        let subject = document.getElementById("edit-subject").value;
+        let cooldownSpanMin = parseInt(document.getElementById("edit-cooldown").value);
+        let sprintSpanMin = parseInt(document.getElementById("edit-sprint").value);
+
+        if (subject != gConfig.subject || cooldownSpanMin != gConfig.cooldownSpanMin || sprintSpanMin != gConfig.sprintSpanMin) {
+            gConfig.subject = subject;
+            gConfig.cooldownSpanMin = cooldownSpanMin;
+            gConfig.sprintSpanMin = sprintSpanMin;
+
+            this.updateSubjectElem();
+            gStatus.prepareSprint();
+        }
+
+        document.getElementById("edit").style.display = "none";
+        document.getElementById("watch").style.display = "block";
+
+        gStatus.startTimer();
+    },
+
+    onClickPause: function() {
+        if (gStatus.ticker == null) {
+            gStatus.startTimer();
+            document.getElementById("button-pause").innerHTML = "=";
+        } else {
+            gStatus.pauseTimer();
+            document.getElementById("button-pause").innerHTML = ">";
+        }
+    },
+
+    onClickSprint: function() {
+        gStatus.startSprint();
+    },
+
+    onClickCooldown: function() {
+        gStatus.startCooldown();
+
+        this.updateStatus();
+        this.updateTimerElem();
+    },
+
+    updateSubjectElem: function() {
+        document.getElementById("subject").innerHTML = gConfig.subject;
+    },
+
+    updateTimerElem: function() {
+        let timeValueSec = gStatus.remainSpanMsec / 1000;
+        let isOvertime = false;
+        if (timeValueSec < 0) {
+            timeValueSec = timeValueSec * -1;
+            isOvertime = true;
+        }
+
+        let timePartHour = Math.floor(timeValueSec / 60 / 60);
+        let timePartMin = Math.floor((timeValueSec - timePartHour * 60 * 60) / 60);
+        let timePartSec = Math.floor(timeValueSec % 60);
+
+        let strHour = timePartHour.toString()
+        if (strHour.length == 1) {
+            strHour = "0" + strHour;
+        }
+        let strMin = timePartMin.toString()
+        if (strMin.length == 1) {
+            strMin = "0" + strMin;
+        }
+        let strSec = timePartSec.toString()
+        if (strSec.length == 1) {
+            strSec = "0" + strSec;
+        }
+
+        var timeStr = strHour + ':' + strMin + ':' + strSec;
+        if (isOvertime) {
+            timeStr = '+' + timeStr;
+        }
+        document.getElementById("timer").innerHTML = timeStr;
+    },
+
+    updateStatus: function() {
+        switch (gStatus.status) {
+            case "cooldown":
+                document.getElementById("button-sprint").style.display = "block";
+                document.getElementById("button-cooldown").style.display = "none";
+                document.body.setAttribute("class", "cooldown");
+                break;
+            case "sprint":
+                document.getElementById("button-sprint").style.display = "none";
+                document.body.setAttribute("class", "sprint");
+                break;
+            case "oversprint":
+                document.getElementById("button-cooldown").style.display = "block";
+                document.body.setAttribute("class", "oversprint");
+                break;
+        }
     }
 }
 
-function onClickSprint() {
-    gStatus.startSprint();
-}
+gStatus.funcUpdateStatus = function() { gFrontend.updateStatus(); };
+gStatus.funcUpdateTimerElem = function() { gFrontend.updateTimerElem(); };
 
-function onClickCooldown() {
-    gStatus.startCooldown();
-
-    updateStatus();
-    updateTimerElem();
-}
-
-function updateSubjectElem() {
-    document.getElementById("subject").innerHTML = gConfig.subject
-}
-
-function updateTimerElem() {
-    let timeValueSec = gStatus.remainSpanMsec / 1000;
-    let isOvertime = false;
-    if (timeValueSec < 0) {
-        timeValueSec = timeValueSec * -1;
-        isOvertime = true;
-    }
-
-    let timePartHour = Math.floor(timeValueSec / 60 / 60);
-    let timePartMin = Math.floor((timeValueSec - timePartHour * 60 * 60) / 60);
-    let timePartSec = Math.floor(timeValueSec % 60);
-
-    let strHour = timePartHour.toString()
-    if (strHour.length == 1) {
-        strHour = "0" + strHour;
-    }
-    let strMin = timePartMin.toString()
-    if (strMin.length == 1) {
-        strMin = "0" + strMin;
-    }
-    let strSec = timePartSec.toString()
-    if (strSec.length == 1) {
-        strSec = "0" + strSec;
-    }
-
-    var timeStr = strHour + ':' + strMin + ':' + strSec;
-    if (isOvertime) {
-        timeStr = '+' + timeStr;
-    }
-    document.getElementById("timer").innerHTML = timeStr;
-}
-
-function updateStatus() {
-    bodyElem = document.body
-
-    switch (gStatus.status) {
-        case "cooldown":
-            document.getElementById("button-sprint").style.display = "block";
-            document.getElementById("button-cooldown").style.display = "none";
-            document.body.setAttribute("class", "cooldown");
-            break;
-        case "sprint":
-            document.getElementById("button-sprint").style.display = "none";
-            document.body.setAttribute("class", "sprint");
-            break;
-        case "oversprint":
-            document.getElementById("button-cooldown").style.display = "block";
-            document.body.setAttribute("class", "oversprint");
-            break;
-    }
-}
-
-gStatus.funcUpdateStatus = updateStatus;
-gStatus.funcUpdateTimerElem = updateTimerElem;
-
-updateSubjectElem();
+gFrontend.updateSubjectElem();
 gStatus.prepareSprint();
 
 gStatus.startTimer();
